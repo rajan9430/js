1fc75b5985432c679a014d2df91df1594d032802
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-04-20 13:43:47 +0000

    Container Config Done

---
+++ js_toturial/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = "9430"
+let accountEmail = "hellotiwari94@gmail.com"
+var accountPassword = "943057"
+accountCity = "Suart"
+let accountState ="Gujarat"
+
+// accountId = "94" const variable in assign value is not change / const values are not change and  not allowed 
+
+// accountEmail = "hello94@gmail.com"
+// accountPassword = "9430"
+// accountCity = "Saran"
+// accountState = "Bihar"
+
+// console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
