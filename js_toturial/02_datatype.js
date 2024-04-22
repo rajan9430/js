1374130be51ab1916ba86ff8a8f7a4df65bfac19
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-04-22 10:32:30 +0000

    data type

---
+++ js_toturial/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello")
+// alert(3+3) // we are using nodejs, not browser
+console.log("Rajan")
+console.log(3
+    +
+    6)// code readability should be high
+
+    
+
+let name = "Rajan" // string data type
+let age = 24 //number data type
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "Rajan")
+log
