25f028accadb5ddd4cc32e937cdb151e2e1e90b4
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-04-24 11:38:34 +0000

    define conversion

--- js_toturial/03_conversionOperation.js
+++ js_toturial/03_conversionOperation.js
@@ -1,6 +1,33 @@
 //let score = 33;
-let score = "33"
+//let score = "33"
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+//let score = false
+//let score = "Rajan"
 
 //const {score} = req.body
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+//false => 0
+
+//let isLoggedIn = 1 // value is  true
+//let isLoggedIn = 0 // value is  false
+//let isLoggedIn = "" // value is false
+// let isLoggedIn = "Rajan" //value is true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
