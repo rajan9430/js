e57fe91f2366c3c0921e89519815b27f4cb85b28
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-04-26 09:39:45 +0000

    Operational operator

---
+++ js_toturial/04_Operations.js
@@ -0,0 +1,37 @@
+//let value = 3
+//let posvalue = +value
+//console.log(posvalue); 
+
+let value = 5
+let nagvalue = -value
+// console.log(nagvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " rajan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //value is 12
+// console.log(1 + "2"); //value is 12
+// console.log("1" + 2 + 2); //value is 122
+// console.log(1 + 2 + "2"); //value is 32
+
+// console.log((3 + 4) *5 % 3); // this value is 2
+
+console.log(true);// value is true
+console.log(false);// value is false
+
+console.log(+true); //value is 1 but this not redebal code/confusition code
+console.log(+""); // parethisas cnvert to boolen and this code result 0 // but this not right code and not redebal code
+
+let gameCounter = 100
+++gameCounter;// this is a prefix operator and this value is 101
+gameCounter++;// this is postfix operator and this value is 101
+console.log(gameCounter);
