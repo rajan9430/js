d860d0bb1b47e8d6744f950d24e4ba37b5fe6fbf
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-05-03 11:06:43 +0000

    comparision

---
+++ js_toturial/05_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1); //this greter than  value is true
+// console.log(2 >= 1);//this value is true
+// console.log(2 < 1);//this value is false
+// console.log(2 == 1);//this value is false
+// console.log(2 != 1);//this notequal value is true
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // null is empity value
+// console.log(null == 0);//false
+// console.log(null >= 0); //true 
+// //please go to the mdn documentation read and clear this consept
+
+// console.log(undefined == 0); // all value is false
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+
+console.log("2" == 2); // this value is true
+console.log("2" === 2); // this value is false
