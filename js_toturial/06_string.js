e8ddaae84af5de1066ba0e801a7b936b18877864
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-05-09 10:51:21 +0000

    all string type

---
+++ js_toturial/06_string.js
@@ -0,0 +1,27 @@
+const name = "Rajan"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // this log use to check the string character 
+
+const gameName = new String("Rajan-ti")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName .toUpperCase());
+console.log(gameName .charAt(2)); // check when number is 2 position on this string?
+console.log(gameName .indexOf('t')); // check where is t position?
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 7)
+console.log(anotherString);// this log result to reverce result
+
+const newStringOne = "   Rajan   "
+console.log(newStringOne); //this string is space string
+console.log(newStringOne.trim()); // this log use trim so remove space this srting
