db14da9abf5cae03af989e4f4773e476e421f17a
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-05-21 10:00:17 +0000

    string_detail

--- js_toturial/06_string.js
+++ js_toturial/06_string.js
@@ -2,10 +2,10 @@ const name = "Rajan"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value"); // this sentex is not redable
-`backtikes`
+// `backtikes`
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // this log use to check the string character 
 
- 
+const gameName = "Mohit-Tiwari"
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,15 +13,23 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // th
 
 // console.log(gameName.length);
 // console.log(gameName .toUpperCase());
-console.log(gameName .charAt(2)); // check when number is 2 position on this string?
-console.log(gameName .indexOf('t')); // check where is t position?
+// console.log(gameName .charAt(2)); // check when number is 2 position on this string?
+// console.log(gameName .indexOf('t')); // check where is t position?
 
-const newString = gameName.substring(0, 4) // substring in not acept - value 
-console.log(newString);
+ const newString = gameName.substring(0, 4) // substring in not acept mainas value 
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 7)
-console.log(anotherString);// this log result to reverce result
+ const anotherString = gameName.slice(-8, 7)
+// console.log(anotherString);// this log result to reverce result
 
-const newStringOne = "   Rajan   "
-console.log(newStringOne); //this string is space string
-console.log(newStringOne.trim()); // this log use trim so remove space this srting
+// const newStringOne = "   Rajan   "
+// console.log(newStringOne); //this string is space string
+// console.log(newStringOne.trim()); // this log use trim so remove space this srting
+
+const url ="https://rajan.com/rajan%20tiwari"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('rajan')); // true
+
+console.log(gameName.split('-')); // This colsole.log result is  ['Mohit', 'Tiwari']
