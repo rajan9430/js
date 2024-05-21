e0fc842980f9fd43f840ed4d4dd409c6873a137b
rajan9430 <104120805+rajan9430@users.noreply.github.com>
2024-05-21 08:27:36 +0000

    datatype_summary

--- js_toturial/datatypes-summary.js
+++ js_toturial/datatypes-summary.js
@@ -20,7 +20,7 @@ let userEmail;
 const id = Symbol ('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 325516184641624n
 
@@ -40,10 +40,39 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof scoreValue);
-console.log(typeof myFunction);
-console.log(typeof heros);
-console.log(typeof anotherId);
-console.log(typeof id);
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof scoreValue);
+// console.log(typeof myFunction);
+// console.log(typeof heros);
+// console.log(typeof anotherId);
+// console.log(typeof id);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) copyvalue, Heap (Non- primitive) refrence value or original value
+
+let myYoutubename = "rajantiwari.com"
+
+
+let anothername = myYoutubename
+anothername = "balcakpubg"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "hellotiwari94@gmail.com",
+    upi: "rajan@ybl"
+}
+
+let userTwo = userOne
+
+console.log(userOne); // This console value is  declier value in let user one
+console.log(userTwo); // this console value is equal user one value
+
+userTwo.email = "rajan@gmail.com"
+
+console.log(userOne.email); // This console value is change the email id
+console.log(userTwo.email);
